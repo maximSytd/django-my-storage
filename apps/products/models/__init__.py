f5600224420574a18df0d8b2b522135149f76488
Maxim Sytdikov <maximovichpro@gmail.com>
2025-04-21 03:42:30 +0500

    Remove employee model

--- apps/products/models/__init__.py
+++ apps/products/models/__init__.py
@@ -2,7 +2,6 @@ from .category import Category
 from .product import Product
 from .supplier import Supplier
 from .shipment import Shipment
-from .employee import Employee
 from .notification import Notification
 from .write_off import WriteOff
 from .departure_point import DeparturePoint
