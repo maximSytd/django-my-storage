993c92e4c8b8339550986464c8f024e0b115ba1f
Maxim Sytdikov <maximovichpro@gmail.com>
2025-05-27 17:36:31 +0500

    Remove List view

--- apps/products/views/supplier.py
+++ apps/products/views/supplier.py
@@ -1,7 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
 
 from django_filters.views import FilterView
 
