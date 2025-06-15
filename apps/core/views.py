e007439b8bd1e3f74db9436e72a9166a44ed8dfe
Maxim Sytdikov <maximovichpro@gmail.com>
2025-06-15 20:21:37 +0500

    Improve design and logic of index page

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,11 +8,27 @@ from django.views.static import serve
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
 
+from apps.products.models import Shipment, DeparturePoint, Supplier, Product
+from apps.users.models import User
+
 class IndexView(LoginRequiredMixin, TemplateView):
     """Class-based view for index page."""
 
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["unaccepted_shipments"] = Shipment.objects.with_contents(
+        ).prefetch_related(
+            "ordered_by",
+        ).exclude(
+            status=Shipment.ShipmentStatus.ACCEPTED.value,
+        )
+        context["departure_points"] = DeparturePoint.objects.count()
+        context["workers_count"] = User.objects.count()
+        context["suppliers_count"] = Supplier.objects.count()
+        context["products_count"] = Product.objects.count()
+        return context
 
 @login_required
 def protected_serve(request, path):
